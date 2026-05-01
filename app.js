09f3ada05e8ad5cfa913857634dcaf8ff291e561
Arpita <bouriarpita05@gmail.com>
2026-05-02 00:45:14 +0530

    fix pakage.json file

--- app.js
+++ app.js
@@ -122,21 +122,14 @@ app.use("/",userRouter);
 app.all(/(.*)/,(req,res,next)=>{
     next(new ExpressError(404,"page not found !"));
  });
-//  app.use((err,req,res,next)=>{
-//     let {statusCode=500,message="something went wrong!"}=err;
-//     // res.status(statusCode).send(message);
-//     res.render("error.ejs",{message});
-// });
-
-app.use((err, req, res, next) => {
-  console.log("🔥🔥🔥 REAL ERROR BELOW 🔥🔥🔥");
-  console.log(err);
-
-  res.send(`
-    <h1>REAL ERROR</h1>
-    <pre>${err.stack}</pre>
-  `);
+ app.use((err,req,res,next)=>{
+    let {statusCode=500,message="something went wrong!"}=err;
+    // res.status(statusCode).send(message);
+    // res.render("error.ejs",{message});
+    res.status(statusCode).render("error.ejs", { message });
 });
+
+
     app.listen(8080,()=>{
     console.log("server is listening to port");
 });
