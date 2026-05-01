8ed6834e7619da2a2dc2968ae7072558777320e8
Arpita <bouriarpita05@gmail.com>
2026-05-01 23:11:01 +0530

    fix pakage.json file

--- controllers/listing.js
+++ controllers/listing.js
@@ -30,9 +30,12 @@ module.exports.index = async(req,res)=>{
  module.exports.createListing = async (req,res,next)=>{
     const newListing = new Listing(req.body.listing);
      newListing.owner = req.user._id; 
-     let url = req.file.path;
-     let filename = req.file.filename;
-     newListing.image ={url,filename};
+     if (req.file) {
+  let url = req.file.path;
+  let filename = req.file.filename;
+  newListing.image = { url, filename };
+}
+    //  newListing.image ={url,filename};
      await newListing.save();
      req.flash("success","new listing created...");
      res.redirect("/listings");
@@ -59,9 +62,14 @@ module.exports.index = async(req,res)=>{
       let {id}=req.params;
       let listing = await Listing.findByIdAndUpdate(id,{...req.body.listing});
 
-      let url = req.file.path;
-      let filename = req.file.filename;
-      listing.image ={url,filename};
+      // let url = req.file.path;
+      // let filename = req.file.filename;
+      // listing.image ={url,filename};
+       if (req.file) {
+  let url = req.file.path;
+  let filename = req.file.filename;
+  listing.image = { url, filename };
+}
       await listing.save();
       req.flash("success","changes made successfully ...");
 
