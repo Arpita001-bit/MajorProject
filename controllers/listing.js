e7aeeabe408ce82f698ff515d4cc90300e5a76b8
Arpita <bouriarpita05@gmail.com>
2026-05-02 00:02:05 +0530

    fix pakage.json file

--- controllers/listing.js
+++ controllers/listing.js
@@ -8,7 +8,7 @@ const Listing = require("../models/listing.js");
 module.exports.index = async(req,res)=>{
    const allListings = await Listing.find({});
    res.render("listings/index.ejs",{allListings});
-    
+  
  };
 
  module.exports.renderNewForm = (req,res)=>{
