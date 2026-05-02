34cc044bd8663ce3956483c4058fe1724b63c728
Arpita <bouriarpita05@gmail.com>
2026-05-02 15:51:03 +0530

    debug logs

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,3 +1,26 @@
+// const cloudinary = require('cloudinary').v2;
+// const {CloudinaryStorage} = require('multer-storage-cloudinary');
+
+// cloudinary.config({
+//     cloud_name:process.env.CLOUD_NAME,
+//     api_key:process.env.CLOUD_API_KEY,
+//     api_secret:process.env.CLOUD_API_SECRET
+// })
+
+// const storage = new CloudinaryStorage({
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: 'wanderlust_DEV',
+//    allowedFormats: ["png","jpg","jpeg"]
+    
+//   },
+// });
+
+// module.exports = {
+//     cloudinary,
+//     storage
+// }
+
 const cloudinary = require('cloudinary').v2;
 const {CloudinaryStorage} = require('multer-storage-cloudinary');
 
@@ -11,7 +34,7 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'wanderlust_DEV',
-   allowedFormats: ["png","jpg","jpeg"]
+   allowedFormats:["png","jpg","jpeg"],
     
   },
 });
