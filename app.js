45c4c7c635ce455e4e66dff086f0a9fcbd0b7ca0
Arpita <bouriarpita05@gmail.com>
2026-06-11 00:50:42 +0530

    fix: remove broken validateListing from app.js

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 
-//   require("dotenv").config();
-// }
-
-// const express= require("express");
-// const mongoose = require("mongoose");
 // const path = require("path");
 // const methodOverride=require("method-override");
 // const ejsMate = require('ejs-mate');
