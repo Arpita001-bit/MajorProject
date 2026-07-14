051430b5cba737d862f57aee05e550b6cf4208a6
Arpita <bouriarpita05@gmail.com>
2026-07-14 23:15:35 +0530

    fix: remove broken validateListing from app.js

--- routes/listings.js
+++ routes/listings.js
@@ -1,7 +1,6 @@
 
 
-// // const multer  = require('multer');
-// // const {storage}= require("../cloudConfig.js");
+
 // // const upload = multer({storage});
 // // const listingController = require("../controllers/listing.js");
 
